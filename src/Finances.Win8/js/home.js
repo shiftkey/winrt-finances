a36bc535af177610e76fcb3e4d2411947900a16e
Brendan Forster <me@brendanforster.com>
2012-03-12 22:03:01 +1100

    added interactive notification dashboard which can be dismissed

--- src/Finances.Win8/js/home.js
+++ src/Finances.Win8/js/home.js
@@ -3,6 +3,7 @@
 
     var ui = WinJS.UI;
     var nav = WinJS.Navigation;
+    var today = new Date();
 
     function menuLink(e) {
         e.preventDefault();
@@ -11,33 +12,26 @@
     };
 
     function showReport(from) {
-        var today = new Date();
         nav.navigate("/html/reports.html", { start: from, end: today } );
     };
 
     function navigateToLastWeek(e) {
         e.preventDefault();
-        var today = new Date();
         var lastweek = new Date().setDate(today.getDate() + 7);
         showReport(lastweek);
     };
 
     function navigateToLastMonth(e) {
         e.preventDefault();
-        var today = new Date();
         var thismonth = new Date(today.getYear(), today.getMonth(), 1, 0, 0, 0, 0);
         showReport(thismonth);
     };
 
     function navigateToThisYear(e) {
         e.preventDefault();
-
-        var today = new Date();
         var startOfYear = new Date(today.getFullYear(), 6, 1, 0, 0, 0, 0); // JS month is 0-11 - July 1
-
         if (startOfYear > today)
             startOfYear.setFullYear(startOfYear.getFullYear() - 1);
-
         showReport(startOfYear);
     }
 
@@ -48,6 +42,20 @@
             element.querySelector("#lastweek").onclick = navigateToLastWeek;
             element.querySelector("#thismonth").onclick = navigateToLastMonth;
             element.querySelector("#yeartodate").onclick = navigateToThisYear;
+
+            var items = element.querySelector(".notifications ul");
+            var width = 0;
+
+            for (var i = 0; i < items.children.length; i++) {
+                var child = items.children[i];
+                width = width + child.offsetWidth;
+            }
+
+            items.style.width = width + "px";
+
+            var notifications = element.querySelector(".notifications");
+            var container = element.querySelector("section[role=main]");
+            element.querySelector("#dismiss").onclick = function () { container.removeChild(notifications); };
         }
     });
 })();
